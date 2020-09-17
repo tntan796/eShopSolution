f582c8263a8e25d6a8509348e10bd29f58d656ba
Tan Tano <tantn1@sunshinegroup.vn>
2020-09-17 15:05:31 +0700

    12 Create application layer structure

---
+++ eShopSolution.Application/Catalog/Products/PublicProductService.cs
@@ -0,0 +1,12 @@
+﻿using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public class PublicProductService
+    {
+    }
+}
